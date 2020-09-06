984791e356ddda527866a9da09aba0b093cfe4d6
Thamjith <jithuthaha8@gmail.com>
2020-09-06 22:54:15 +0530

    redux incomplete

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,27 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 import './App.css';
+import { Provider } from 'react-redux';
+import store from './store';
 
 const App = () => 
-  <Fragment>
-    <h1>App</h1>
-  </Fragment>
-
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path='/' component={Landing} />
+        <section>
+          <Switch>
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
 
 export default App;
